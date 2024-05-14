242dd28f424e340ed79c8d9587873e935efedcfa
Aditya Umale <aditya.umale17@gmail.com>
2024-05-14 21:14:52 +0530

    frontend done

---
+++ frontend/src/courses/Courses.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Course from '../components/Course'
+import Footer from '../components/Footer'
+
+
+function Courses() {
+    
+  return (
+    <>
+    <Navbar />
+    <Course />
+    <Footer />
+    </>
+  )
+}
+
+export default Courses
